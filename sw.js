80483807b03f98f829e0855a40b0f05294d8e6eb
heididiazs1986-hash <heidi.diazs1986@gmail.com>
2026-02-18 06:10:43 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = "gsdx-cache-v1";
-const ASSETS = ["./","./index_funcional.html","./manifest.json","./LOGO.jpg","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-180.png"];
-self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match("./index_funcional.html"))));});
