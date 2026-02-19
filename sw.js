e25f2408d69cde8bfd48e93a23e1e01353e278e3
heididiazs1986-hash <heidi.diazs1986@gmail.com>
2026-02-18 23:13:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,5 @@
-const CACHE_NAME = "gsdx-cache-v4";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js",
-  "/LOGO.jpg",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/icon-180.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((c) => c.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => {
-      const fetchPromise = fetch(e.request)
-        .then((netRes) => {
-          if (netRes && netRes.status === 200) {
-            const copy = netRes.clone();
-            caches.open(CACHE_NAME).then((c) => c.put(e.request, copy));
-          }
-          return netRes;
-        })
-        .catch(() => cached || caches.match("/index.html"));
-      return cached || fetchPromise;
-    })
-  );
-});
+const CACHE_NAME = "gsdx-cache-v3";
+const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./LOGO.jpg","./icon-192.png","./icon-512.png","./icon-180.png"];
+self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match("./index.html"))));});
